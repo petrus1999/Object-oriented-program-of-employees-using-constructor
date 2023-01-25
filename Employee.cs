3ad67273a06fcadfacea3f6e3687a326e04a65a2
petrus <pshai671@gmail.com>
2023-01-25 09:25:36 +0200

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_oriented__program_which_print_out_employee_details
+{
+    class Employee
+    {
+        public string Name;
+        public string Surname;
+        public string Gender;
+        public int Dateofbirth;
+        public string Nationality;
+        public int Age;
+        public string Position;
+
+        public Employee(string name, string surname, string gender, int dateofbirth, string nationality, string position, int age) {
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+            Dateofbirth = dateofbirth
+            Nationality = nationality;
+            Age = age;
+            Position = position;
+
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Object_oriented__program_which_print_out_employee_details;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee person1 = new Employee("PETRUS", "SHAI", "MALE", 1999 / 09 / 13, "SOUTH AFRICAN", "CEO", 24);
+        Employee person2 = new Employee("PRECIOUS", "MPURU", "FEMALE", 2000 / 02 / 25, "SOUTH AFRICAN", "COO", 23);
+        Employee person3 = new Employee("THULI", "BALOYI", "FEMALE", 1999 / 10 / 21, "SOUTH AFRICAN", "SUPERVISOR", 24);
+        Employee person4 = new Employee("MALESELA", "LETSOALO", "MALE", 1994 / 09 / 07, "SOUTH AFRICAN", "MANAGER", 30);
+
+        Console.WriteLine(person1.Name + " "+ person1.Surname+" " +person1.Gender+" "+ person1.Dateofbirth+" "+ person1.Nationality+" "+ person1.Position+" "+ person1.Age);
+        Console.WriteLine(person2.Name + " " + person2.Surname + " " + person2.Gender + " " + person2.Dateofbirth + " " + person2.Nationality + " " + person2.Position + " "+ person2.Age);
+        Console.WriteLine(person3.Name + " " + person3.Surname + " " + person3.Gender + " " + person3.Dateofbirth + " " + person3.Nationality + " " + person3.Position + " "+ person3.Age);
+        Console.WriteLine(person4.Name + " " + person4.Surname + " " + person4.Gender + " " + person4.Dateofbirth + " " + person4.Nationality + " " + person4.Position + " "+ person4.Age);
+    }
+}
+    
